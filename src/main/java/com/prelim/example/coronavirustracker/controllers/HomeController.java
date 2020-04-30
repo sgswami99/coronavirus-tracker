cbd9c4892413959f8d0af977f02517b31d77b2ef
Saurav Goswami <48852209+sgswami99@users.noreply.github.com>
2020-04-30 16:41:00 +0530

    Add files via upload

--- src/main/java/com/prelim/example/coronavirustracker/controllers/HomeController.java
+++ src/main/java/com/prelim/example/coronavirustracker/controllers/HomeController.java
@@ -1,5 +1,7 @@
 package com.prelim.example.coronavirustracker.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.prelim.example.coronavirustracker.models.LocationStats;
 import com.prelim.example.coronavirustracker.services.CoronaVirusDataService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Controller
 public class HomeController {
@@ -17,9 +21,21 @@ public class HomeController {
     @GetMapping("/")
     public  String home(Model model){
         List<LocationStats> allStats=coronaVirusDataService.getAllstats();
+        Map<String, Integer> totalCaseGroupedbyCountry =
+                allStats.stream().collect(Collectors.groupingBy(LocationStats::getCountry,
+                        Collectors.summingInt(LocationStats::getLatestTotalCases)));
         int totalReportedCases=allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
         int totalNewCases=allStats.stream().mapToInt(stat -> stat.getDiffFromPrevDay()).sum();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String strTotalCaseGroupedbyCountry = null;
+        try {
+            strTotalCaseGroupedbyCountry = objectMapper.writeValueAsString(totalCaseGroupedbyCountry);
+        } catch (JsonProcessingException e) {
+            strTotalCaseGroupedbyCountry = "";
+        }
         model.addAttribute("locationStats", allStats);
+        model.addAttribute("totalCaseGroupedbyCountry", strTotalCaseGroupedbyCountry);
         model.addAttribute("totalReportedCases", totalReportedCases);
         model.addAttribute("totalNewCases", totalNewCases);
         return "home";
